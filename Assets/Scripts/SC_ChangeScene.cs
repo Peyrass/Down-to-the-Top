58ff91559d28fe8e750e709367c4394649303205
klauuu <claudia.gamez@live.u-tad.com>
2026-04-25 19:12:50 +0200

    Cambio Escena

---
+++ Assets/Scripts/SC_ChangeScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class SC_ChangeScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    
+}
