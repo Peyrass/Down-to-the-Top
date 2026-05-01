c26aa4d7e7b5f9f65b158f0a71a42cd02917d1c4
klauuuga <claudia.gamez@live.u-tad.com>
2026-05-01 18:11:32 +0200

    Lógica muñeca, hachas y lvl3

---
+++ Assets/Scripts/Enemys/Level3/SC_DollBehaviour.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SC_DollBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    [SerializeField] private Transform targetToFollow;
+
+    [Header("Attack Behavior")]
+    [SerializeField] private float explosionRadius;
+    [SerializeField] private LayerMask whatIsDamagable;
+
+    [SerializeField] int secondsToExplosion = 4;
+    private float leftTimeToExplosion;
+    private bool countingExplosion = false;
+
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(targetToFollow.position);
+
+        if (DestinationReached()) //si estas listo y ... PREGUNTA DE EXAMEN
+        {
+            LookToTarget();
+            animator.SetBool("Reached", true);
+            agent.isStopped = true; //Me aseguro de estar quieto mientras lanzo el ataque
+        }
+    }
+
+    private void LookToTarget()
+    {
+        Vector3 direction = (targetToFollow.position - transform.position);
+        direction.y = 0f;
+        transform.rotation = Quaternion.LookRotation(direction); //le das una direccion y lo transforma en una rotacion
+    }
+    private bool DestinationReached()
+    {
+        return !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartExplosion(secondsToExplosion);
+        }
+    }
+
+    private void StartExplosion(int sec)
+    {
+        leftTimeToExplosion = sec;
+        countingExplosion = true;
+        Debug.Log("Explosion started! Time to explosion: ");
+        StartCoroutine(ExplosionCountdown());
+    }
+
+    IEnumerator ExplosionCountdown()
+    {
+        while (leftTimeToExplosion > 0)
+        {
+            leftTimeToExplosion -= Time.deltaTime;
+            yield return null;
+        }
+        countingExplosion = false;
+        Debug.Log("BOOM!");
+        // Añadir animación de explosión y daño a jugador
+        Destroy(gameObject); 
+    }
+}
