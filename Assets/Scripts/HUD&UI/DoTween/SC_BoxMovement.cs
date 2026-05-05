21b138db30775ae98414af6d9c763d251b198d83
klauuuga <claudia.gamez@live.u-tad.com>
2026-05-05 17:09:43 +0200

    Target Animation Level

--- Assets/Scripts/HUD&UI/DoTween/SC_BoxMovement.cs
+++ Assets/Scripts/HUD&UI/DoTween/SC_BoxMovement.cs
@@ -3,15 +3,11 @@ using DG.Tweening;
 
 public class SC_BoxMovement : MonoBehaviour
 {
+    [SerializeField] private Transform target; 
     [SerializeField] private float _cycleLength = 2f;
     void Start()
     {
         //transform.DOMoveX(6, 0).SetDelay(1f).SetEase(Ease.InOutSine);
-        transform.DOMove(new Vector3(6, 0, 0), _cycleLength).SetDelay(1f);
-    }
-
-    void Update()
-    {
-        
+        transform.DOMove(target.position, _cycleLength).SetDelay(1f);
     }
 }
